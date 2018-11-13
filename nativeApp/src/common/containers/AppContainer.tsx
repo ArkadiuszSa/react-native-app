3e87e0e065815354ab462aeec0d15a9b101a53a0
Arek <sapek.arkadiusz@gmail.com>
2018-11-13 10:18:03 +0100

    [#11710]-add loading Icons

--- nativeApp/src/common/containers/AppContainer.tsx
+++ nativeApp/src/common/containers/AppContainer.tsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Root } from 'native-base';
+import { Font } from 'expo';
 
 import { RootNavigator } from '../components/RootNavigator';
 
 export class AppContainer extends Component {
+public state= {
+  isReady: false,
+}
+  public componentDidMount() {
+    this.loadFonts();
+  }
+  async loadFonts() {
+    await Font.loadAsync({
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+    });
+    this.setState({ isReady: true });
+  }
   render() {
     return (
       <Root>
-        <RootNavigator />
+        {this.state.isReady && <RootNavigator />}
       </Root>
     );
   }
